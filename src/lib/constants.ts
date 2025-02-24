eff2880e6970e330813e748a44ea6ea29b159480
codevski <1435321+codevski@users.noreply.github.com>
2025-02-24 19:29:05 +1100

    ♻️ refactor(config): reduce relays to avoid current bug/error on loading vids

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,11 @@
 export const DEFAULT_RELAYS = [
   "wss://relay.damus.io",
-  "wss://relay.snort.social",
-  "wss://nos.lol",
-  "wss://relay.j35tr.com",
-  "wss://relay.nostr.band",
-  "wss://nostr.mom",
-  "wss://relay.0xchat.com",
+  // "wss://relay.snort.social",
+  // "wss://nos.lol",
+  // "wss://relay.j35tr.com",
+  // "wss://relay.nostr.band",
+  // "wss://nostr.mom",
+  // "wss://relay.0xchat.com",
 ];
 
 export const VALID_VIDEO_EXTENSIONS = [".mp4", ".webm", ".mov", ".m4v"];
